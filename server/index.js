5fc9f6e3c9dbed3ab56bac4cf7df958f0e0a2ee4
Nabin Karki <karki0008@gmail.com>
2025-02-03 20:52:38 +0545

    UI redesigned

--- server/index.js
+++ server/index.js
@@ -1,80 +1,59 @@
 const express = require("express");
 const app = express();
-app.use(express.json)
+app.use(express.json);
 const port = 9000;
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/glamorizeDb");
 
-const { Schema }=mongoose;
+const { Schema } = mongoose;
 const userSchema = new Schema(
   {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // Email format validation
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 6,
-    },
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     firstName: {
       type: String,
       required: true,
+      trim: true,
     },
     lastName: {
       type: String,
       required: true,
+      trim: true,
     },
-    phoneNumber: {
+    email: {
       type: String,
-      match: /^\+\d{1,15}$/, // International phone number validation
-      required: false,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    bodyMeasurements: {
-      height: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      weight: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      chest: {
-        type: Number,
-        required: false,
-        min: 0,
-      },
-      waist: {
-        type: Number,
-        required: false,
-        min: 0,
-      },
-      hips: {
-        type: Number,
-        required: false,
-        min: 0,
-      },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmPassword: {
+      type: String,
+      required: true,
+    },
+    acceptTerms: {
+      type: Boolean,
+      required: true,
     },
   },
-); 
-const User= mongoose.model("User", userSchema);
+  { timestamps: true }
+);
 
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/", (req, res) => {
- User.create(req.body);
+app.post("/register", (req, res) => {
+  User.create(req.body);
 });
 
 app.listen(port, () => {
